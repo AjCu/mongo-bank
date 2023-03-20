1f1b5e52752ea6588e4ce1511e1637f8dc981ef3
Alberto Cristancho <albertocristancho@outlook.com>
2023-03-20 18:16:12 -0500

    build: changed import in main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { TransfersModule } from './transfers/transfers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI_STRING_CONNECTION || ''),
+    UsersModule,
+    TransfersModule,
+  ],
 })
 export class AppModule {}
